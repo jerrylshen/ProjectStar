077e0e22771f3fb8eab332dee00ad4b0201caf38
Howard Lam <howardlam546@yahoo.com>
2017-11-03 17:51:38 -0700

    Implemented basics
    Changes to player movement (left, right), camera movement, and placement of scene objects

--- ProjectStar/Assets/Scripts/Movement.cs
+++ ProjectStar/Assets/Scripts/Movement.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    public float speed = .5f;
+
 	// Use this for initialization
 	void Start () {
+        
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(speed * Time.deltaTime + transform.position.x, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+
+    }
 }
---
+++ ProjectStar/Assets/cameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(transform.parent.transform.position.x,
+            transform.parent.transform.position.y, -2);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
