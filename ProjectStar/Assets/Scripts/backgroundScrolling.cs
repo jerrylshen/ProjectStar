ca0aa922e4ac703a037f00584e6fe6195d073a53
Howard Lam <howardlam546@yahoo.com>
2017-11-05 20:36:50 -0800

    Added parallax scrolling

---
+++ ProjectStar/Assets/Scripts/backgroundScrolling.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundScrolling : MonoBehaviour {
+
+    public float smoothspeed = 1f;
+    public Transform cam;
+    private Vector3 previousCamPos;
+    private float parallaxNum = -50;
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        float parallax = (previousCamPos.x - cam.position.x) * parallaxNum;
+        float backgroundTargetPosX = transform.position.x + parallax;
+
+        Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, transform.position.y,
+        transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, backgroundTargetPos, smoothspeed * Time.deltaTime);
+        
+
+        previousCamPos = cam.position;
+    }
+}
