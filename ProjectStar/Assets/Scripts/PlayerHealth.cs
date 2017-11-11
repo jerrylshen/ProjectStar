489abed0111a9c35c053ae588cdd3a6473b02e3f
Jerry Shen <jerrylshen@gmail.com>
2017-11-10 17:20:53 -0800

    health, hunger, stress meters
    Added health, hunger, stress meters

---
+++ ProjectStar/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public Slider healthSlider;                                 // Reference to the UI's health bar.
+
+    bool isDead;                                                // Whether the player is dead.
+    bool damaged;                                               // True when the player gets damaged.
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = startingHealth;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount; // Reduce the current health by the damage amount.
+
+        // If the player has lost all it's health
+        if (currentHealth <= 0)
+        {
+            // ... it should die.
+            //Death();
+        }
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+    }
+}
